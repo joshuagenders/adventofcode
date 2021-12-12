4511c899a466afedc0b10747a82b4fd19bcb94c7
Joshua Genders <joshuagenders@hotmail.com>
2021-12-12 01:37:11 +0000

    sol3

---
+++ 3/sol.py
@@ -0,0 +1,5 @@
+input_list = [ '00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010' ]
+transposed = [[val[bit_position] for val in input_list] for bit_position in range(5)]
+most_common = int(''.join(['1' if val.count('1') > val.count('0') else '0' for val in transposed]), 2)
+not_most_common = ~most_common & int('11111', 2)
+print(most_common * not_most_common)
