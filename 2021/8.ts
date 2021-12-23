5b4bd644a46ea5765174490323b178cabf09b0cf
Joshua Genders <joshuagenders@hotmail.com>
2021-12-23 20:04:18 +0000

    fix

--- 2021/8.ts
+++ 2021/8.ts
@@ -9,14 +9,11 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`;
 
-const uniqueLengths = (words: string[]) =>
-    words
+const uniqueLengths = (words: string[]) => {
+    const lengths = words
         .map(word => word.length)
-        .reduce(
-            (acc: number[], wordLength: number) => acc.includes(wordLength)
-                ? acc.filter(w => w !== wordLength)
-                : [wordLength, ...acc]
-        , [])
+    return lengths.filter(l => lengths.filter(r => r == l).length == 1)
+}
 
 const parser = (line: string) : string[][] =>
     line.split('|').map(v => v.trim()).map(l => l.split(' '))
@@ -26,7 +23,12 @@ const sumOfUniqueCombinations = parsed.map(line => {
     const combinations = line[0]
     const output = line[1]
     const counts = uniqueLengths(combinations)
+    console.log(combinations)
+    console.log(counts)
+    console.log(output)
     const countOfUnique = output.filter(o => counts.includes(o.length)).length
+    console.log(countOfUnique)
+    console.log("===============")
     return countOfUnique
 }).reduce((acc, v) => acc + v, 0)
 console.log(sumOfUniqueCombinations)
