7728a0be42196c06c3ee44df2b17617dec8dd4d4
joshuagenders <joshuagenders@hotmail.com>
2022-12-18 16:57:42 +1100

    terrible sol9 p2

--- 2022/9/solution_9.py
+++ 2022/9/solution_9.py
@@ -53,3 +53,67 @@ for line in raw_input.splitlines():
                 y -= 1
         visited[f'{x},{y}'] = True
 print(len(visited))
+
+# raw_input = '''R 5
+# U 8
+# L 8
+# D 3
+# R 17
+# D 10
+# L 25
+# U 20'''
+
+visited = {}
+rope = [[0,0] for _ in range(10)] 
+print(rope)
+for line in raw_input.splitlines():
+    [direction, steps] = line.split(' ')
+    steps = int(steps)
+
+    # there is a much better way to do this...presumably with some vector math
+    for step in range(steps):
+        for index, v in enumerate(rope):
+            if index == 0:
+                if direction == 'U':
+                    v[1] += 1
+                if direction == 'D':
+                    v[1] -= 1
+                if direction == 'L':
+                    v[0] -= 1
+                if direction == 'R':
+                    v[0] += 1
+                continue
+            x = v[0]
+            y = v[1]
+            hx = rope[index -1][0]
+            hy = rope[index -1][1]
+            dx = abs(hx - x)
+            dy = abs(hy - y)
+            are_adjecent = (not (dx > 1 or dy > 1)) or (dx == 1 and dy == 1)
+            # If the head is ever two steps directly up, down, left, or right from the tail, the tail must also move one step in that direction so it remains close enough:
+            if hx == x + 2 and hy == y:
+                x += 1
+            elif hx == x - 2 and hy == y:
+                x -= 1
+            elif hy == y + 2 and hx == x:
+                y += 1
+            elif hy == y - 2 and hx == x:
+                y -= 1
+            # Otherwise, if the head and tail aren't touching and aren't in the same row or column, the tail always moves one step diagonally to keep up:
+            elif not are_adjecent:
+                if hx > x:
+                    x += 1
+                else:
+                    x -= 1
+                if hy > y:
+                    y += 1
+                else:
+                    y -= 1
+            rope[index][0] = x
+            rope[index][1] = y
+            if index == 9:
+                visited[f'{x},{y}'] = True
+
+print(len(visited))
+
+
