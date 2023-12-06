623c497f4c99074e76c5d4251ffcb13139ebaeb9
joshuagenders <joshuagenders@hotmail.com>
2023-12-06 11:05:58 +1100

    part2 solution

---
+++ 2023/4/part2.py
@@ -0,0 +1,36 @@
+with open('data.txt', 'r') as fh:
+  input_raw = fh.read()
+
+# input_raw = '''Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+# '''
+
+
+games = [
+  map(
+    lambda n: set(filter(bool, n.strip().split(' '))),
+    w[1].split('|')
+  ) for w in [
+    line.split(':') for line in input_raw.splitlines()
+  ]
+]
+winning = []
+for a in games:
+  b = list(a)
+  l = len(b[0] & b[1])
+  winning.append(l)
+
+copies = [1]*len(games)
+i = 0
+while (i < len(copies)):
+  amount = winning[i]
+  for _ in range(copies[i]):
+    for card in range(i + 1, i + 1 + amount):
+      copies[card] += 1
+  i += 1
+
+print(sum(copies))
